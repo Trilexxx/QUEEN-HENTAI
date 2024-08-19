e3cc1332aab4b4e4be49a551efa701e32b9ef017
Trilexxx <125469321+Trilexxx@users.noreply.github.com>
2024-08-19 06:02:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['237620914677', 'trilex²⁰²³', true],
+  ['237620914677', 'trilex²⁰²³', true], 
   [''] 
 ] //Number of owners
 
